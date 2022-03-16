d54790c77106acf63091f889fc17fad8a2fc724f
Pratik Bambulkar <72552513+Pratik180198@users.noreply.github.com>
2022-03-17 01:34:05 +0530

    commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return {'data': {'name': 'Pratik'}}
+
+
+@app.get('/about')
+def about():
+    return {'data': 'about page'}
+
+
+@app.get('/about/users{no}')
+def users(no):
+    return {'users': f'There are {no} of users'}
+
+
+@app.get('/home')
+def home(limit: int = 10, published: bool = True):
+    if published:
+        return f"This is {limit} and published is True"
+    else:
+        return f"This is {limit} and published is False"
+
+
